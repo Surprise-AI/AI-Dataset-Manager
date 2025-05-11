b99f06bf1e59416af5f6c0dac1bbe045816f282f
Mohammad Hashim <88169374+uvbj@users.noreply.github.com>
2025-05-11 06:02:49 +0300

    data_format_support.py

--- data_format_support.py
+++ data_format_support.py
@@ -2,21 +2,17 @@ import json
 import pandas as pd
 
 def load_json(file_path):
-    
     with open(file_path, 'r') as file:
         data = json.load(file)
     return data
 
 def save_json(data, file_path):
-   
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=4)
 
 def load_csv(file_path):
-   
     data = pd.read_csv(file_path)
     return data
 
 def save_csv(data, file_path):
-    
     data.to_csv(file_path, index=False)
